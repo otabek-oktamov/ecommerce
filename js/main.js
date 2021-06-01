5785411c57810417af5dd255305336dbe9a27340
otabek-oktamov <otabek20040608@gmail.com>
2021-06-01 09:17:15 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.querySelector('.burger').addEventListener('click',()=>{
+document.querySelector('.burger').classList.toggle('clicked')
+document.querySelector('.site-header').classList.toggle('active')
+document.querySelector('.site-header__form').classList.toggle('df')
+document.querySelector('.site-header__top').classList.toggle('db')
+document.querySelector('.site-header__bottom').classList.toggle('db')
+})
