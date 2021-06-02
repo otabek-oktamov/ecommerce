d529e5e2f4afd8b016c4abc580291ba2e314026a
otabek-oktamov <otabek20040608@gmail.com>
2021-06-02 13:24:49 +0300

    initial comit

--- js/main.js
+++ js/main.js
@@ -5,12 +5,14 @@ document.querySelector('.site-header__form').classList.toggle('df')
 document.querySelector('.site-header__top').classList.toggle('db')
 document.querySelector('.site-header__bottom').classList.toggle('db')
 document.body.classList.toggle('scroll')
-})
 document.querySelector('.search-btn').addEventListener('click',()=>{
+  document.querySelector('.search-btn').classList.toggle('close')
   document.querySelector('.search').classList.toggle('db')
   document.querySelector('.site-header__select-menu').classList.toggle('dn')
   document.querySelector('.user').classList.toggle('dn')
 })
+})
+
 $(document).ready(function() {
   $('#autoWidth').lightSlider({
       autoWidth:true,
