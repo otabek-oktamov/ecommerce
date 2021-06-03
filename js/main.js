473b14b20685e6d73f59991fb4c653175614d489
otabek-oktamov <otabek20040608@gmail.com>
2021-06-03 10:22:01 +0300

    initial commot

--- js/main.js
+++ js/main.js
@@ -1,34 +1,62 @@
-document.querySelector('.burger').addEventListener('click', () => {
-  document.querySelector('.burger').classList.toggle('clicked')
-  document.querySelector('.site-header').classList.toggle('active')
-  document.querySelector('.site-header__form').classList.toggle('df')
-  document.querySelector('.site-header__top').classList.toggle('db')
-  document.querySelector('.site-header__bottom').classList.toggle('db')
-  document.body.classList.toggle('scroll')
-  document.querySelector('.search-btn').addEventListener('click', () => {
-    document.querySelector('.search-btn').classList.toggle('close')
-    document.querySelector('.search').classList.toggle('db')
-    document.querySelector('.site-header__select-menu').classList.toggle('dn')
-    document.querySelector('.user').classList.toggle('dn')
+window.addEventListener('DOMContentLoaded', () => {
+  function mobileNav() {
+    let burgerBtn = document.querySelector('.burger')
+        siteHeader = document.querySelector('.site-header')
+        siteHeaderForm = document.querySelector('.site-header__form')
+        siteHeaderTop = document.querySelector('.site-header__top')
+        siteHeaderBottom = document.querySelector('.site-header__bottom')
+        searchBtn = document.querySelector('.search-btn')
+
+        burgerBtn.addEventListener('click',()=>{
+          burgerBtn.classList.toggle('clicked')
+          siteHeader.classList.toggle('active')
+          siteHeaderForm.classList.toggle('df')
+          siteHeaderTop.classList.toggle('db')
+          siteHeaderBottom.classList.toggle('db')
+          document.body.classList.toggle('scroll')
+
+
+          searchBtn.addEventListener('click',()=>{
+            searchBtn.classList.toggle('close')
+            document.querySelector('.search').classList.toggle('db')
+            document.querySelector('.site-header__select-menu').classList.toggle('dn')
+            document.querySelector('.user').classList.toggle('dn')
+          })
+        })
+
+  }
+  mobileNav()
+
+  function disabled(){
+let homePage = document.querySelector('.f-home-nav__home-page');
+    fruitPage =  document.querySelector('.f-home-nav__fruit-page');
+
+    grid = document.querySelector('.f-grid')
+    list = document.querySelector('.f-list')
+
+    homePage.addEventListener('click',()=>{
+      homePage.classList.remove('disabled')
+      fruitPage.classList.add('disabled')
+    })
+
+    fruitPage.addEventListener('click',()=>{
+      fruitPage.classList.remove('disabled')
+      homePage.classList.add('disabled')
+    })
+
+  grid.addEventListener('click',()=>{
+    grid.classList.remove('disabled')
+    list.classList.add('disabled')
   })
+  list.addEventListener('click',()=>{
+    list.classList.remove('disabled')
+    grid.classList.add('disabled')
+  })
+  }
+  disabled()
 })
 
-document.querySelector('.f-home-nav__home-page').addEventListener('click', () => {
-  document.querySelector('.f-home-nav__home-page').classList.remove('disabled')
-  document.querySelector('.f-home-nav__fruit-page').classList.add('disabled')
-})
-document.querySelector('.f-home-nav__fruit-page').addEventListener('click', () => {
-  document.querySelector('.f-home-nav__fruit-page').classList.remove('disabled')
-  document.querySelector('.f-home-nav__home-page').classList.add('disabled')
-})
-document.querySelector('.f-grid').addEventListener('click', () => {
-  document.querySelector('.f-grid').classList.remove('disabled')
-  document.querySelector('.f-list').classList.add('disabled')
-})
-document.querySelector('.f-list').addEventListener('click', () => {
-  document.querySelector('.f-list').classList.remove('disabled')
-  document.querySelector('.f-grid').classList.add('disabled')
-})
+
 $(document).ready(function () {
   $('#autoWidth').lightSlider({
     autoWidth: true,
@@ -38,3 +66,17 @@ $(document).ready(function () {
     }
   });
 });
+
+
+let favorite = document.querySelectorAll('.wish-list');
+
+favorite.forEach(item => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('favorite')
+  })
+});
+
+
+
+
+
