7b7ca7a9d41d8468ed227d33d5936687306f19eb
otabek-oktamov <otabek20040608@gmail.com>
2021-06-03 10:31:39 +0300

    hidden

--- js/main.js
+++ js/main.js
@@ -78,5 +78,36 @@ favorite.forEach(item => {
 
 
 
+let min = 10;
+let max = 100;
+
+const calcLeftPosition = (value) => (100 / (100 - 10)) * (value - 10);
+
+$("#rangeMin").on("input", function (e) {
+  const newValue = parseInt(e.target.value);
+  if (newValue > max) return;
+  min = newValue;
+  $("#thumbMin").css("left", calcLeftPosition(newValue) + "%");
+  $("#min").html(newValue);
+  $("#line").css({
+    left: calcLeftPosition(newValue) + "%",
+    right: 100 - calcLeftPosition(max) + "%"
+  });
+});
+
+$("#rangeMax").on("input", function (e) {
+  const newValue = parseInt(e.target.value);
+  if (newValue < min) return;
+  max = newValue;
+  $("#thumbMax").css("left", calcLeftPosition(newValue) + "%");
+  $("#max").html(newValue);
+  $("#line").css({
+    left: calcLeftPosition(min) + "%",
+    right: 100 - calcLeftPosition(newValue) + "%"
+  });
+});
+
+
+
 
 
